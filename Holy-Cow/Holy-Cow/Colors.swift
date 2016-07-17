2317cd0583bb6ae61f3ae8c20116381d775fac0a
nadiajam <nadiajamrozik2018@u.northwestern.edu>
2016-07-18 01:48:02 +0200

    fixed colors.swift problem (fonts still not showing up)

---
+++ Holy-Cow/Holy-Cow/Colors.swift
@@ -0,0 +1,41 @@
+//
+//  Colors2.swift
+//  Holy-Cow
+//
+//  Created by Nadia Jamrozik on 7/18/16.
+//  Copyright © 2016 Nadia Jamrozik. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    class var holyBlue: UIColor {
+        return UIColor(red: 3.0/255.0, green: 196.0/255.0, blue: 251.0/255.0, alpha: 1.0)
+    }
+    
+    class var holyGreen: UIColor {
+        return UIColor(red: 0.0/255.0, green: 206.0/255.0, blue: 157.0/255.0, alpha: 1.0)
+    }
+    
+    class var holyPurple: UIColor {
+        return UIColor(red: 82.0/255.0, green: 39.0/255.0, blue: 108.0/255.0, alpha: 1.0)
+    }
+    
+    class var holyOrange: UIColor {
+        return UIColor(red: 255.0/255.0, green: 171.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+    }
+    
+    class var holyRed: UIColor {
+        return UIColor(red: 254.0/255.0, green: 86.0/255.0, blue: 83.0/255.0, alpha: 1.0)
+    }
+    
+    class var holyBlack: UIColor {
+        return UIColor(red: 34.0/255.0, green: 31.0/255.0, blue: 26.0/255.0, alpha: 1.0)
+    }
+    
+    class var holyGrey: UIColor {
+        return UIColor(red: 189.0/255.0, green: 203.0/255.0, blue: 212.0/255.0, alpha: 1.0)
+    }
+    
+}
