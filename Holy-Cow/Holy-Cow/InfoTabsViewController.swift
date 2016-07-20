ed7a52c2a175543059cecfc4cebd03b6eb823d18
Marky <maria_pertsemlidi@brown.edu>
2016-07-20 10:47:02 +0200

    onboarding screens almost done

--- Holy-Cow/Holy-Cow/InfoTabsViewController.swift
+++ Holy-Cow/Holy-Cow/InfoTabsViewController.swift
@@ -9,86 +9,33 @@
 import UIKit
 import Foundation
 
-class InfoTabsViewController: UIPageViewController, UIPageViewControllerDelegate {
+extension UIViewController {
     
-    //colors of info tabs in order of tabs appearance
-    let colorsArray = [UIColor.holyPurple, UIColor.holyBlue, UIColor.holyOrange, UIColor.holyGreen, UIColor.whiteColor()]
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        dataSource = self
-        //loads the first view controller
-        if let firstInfoTab = infoTabsArray.first {
-            setViewControllers([firstInfoTab], direction: .Forward, animated: true, completion: nil)
-        }
-        
-//        self.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height - 30)
-    }
-
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        for i in 0..<infoTabsArray.count {
-            infoTabsArray[i].view.backgroundColor = colorsArray[i]
-            infoTabsArray[i].view.frame = UIScreen.mainScreen().bounds
-//            infoTabsArray[i].
-        }
+    @IBAction func dismissAction(sender: AnyObject) {
+        navigationController?.popViewControllerAnimated(true)
     }
     
-    //Instantiates ordered array variable of infoTabs
-    private(set) var infoTabsArray:[UIViewController] = {
-        //local function - takes in a string name that identifies which tab it is
-        func newInfoTab(tabNum: String) -> UIViewController {
-            return UIStoryboard(name: "InfoTabs", bundle: nil).instantiateViewControllerWithIdentifier("\(tabNum)Tab")
-        }
-        //returns array of ordered infoTabs
-        return [newInfoTab("First"), newInfoTab("Second"), newInfoTab("Third"), newInfoTab("Fourth"), newInfoTab("Fifth")]
-    }()
 }
 
 
-extension InfoTabsViewController : UIPageViewControllerDataSource {
+class InfoTabsController: UIViewController {
     
-    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
-        
-        guard let infoTabIndex = infoTabsArray.indexOf(viewController) else {
-            return nil
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        for subview in view.subviews {
+            if let textField = subview as? UILabel {
+                if textField.tag == 1 { //tag 1 = titles
+                    textField.font = UIFont(name: "GTWalsheimProTrial-Bold", size: 40.0)
+                } else if textField.tag == 2 { //tag 2 = numbers
+                    textField.font = UIFont(name: "GTWalsheimProTrial-Bold", size: 60.0)
+                } else if textField.tag == 3 { //equal signs
+                    textField.font = UIFont(name: "GTWalsheimProTrial-Regular", size: 60.0)
+                } else { //standard text
+                    textField.font = UIFont(name: "GTWalsheimProTrial-Regular", size: 27.0)
+                }
+                
+            }
         }
-        let previousIndex = infoTabIndex - 1
-        guard previousIndex >= 0 else {
-            return nil
-        }
-        
-        return infoTabsArray[previousIndex]
     }
-    
-    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
-        
-        guard let infoTabIndex = infoTabsArray.indexOf(viewController) else {
-            return nil
-        }
-        let nextIndex = infoTabIndex + 1
-        let tabsCount = infoTabsArray.count
-        guard tabsCount != nextIndex else {
-            return nil
-        }
-        guard tabsCount > nextIndex else {
-            return nil
-        }
-        
-        return infoTabsArray[nextIndex]
-    }
-    
-//    //need for dots
-//    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
-//        return infoTabsArray.count
-//    }
-//    
-//    //need for dots
-//    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
-//        guard let firstTab = viewControllers?.first, firstTabIndex = infoTabsArray.indexOf(firstTab) else {
-//            return 0
-//        }
-//        return firstTabIndex
-//    }
     
 }
