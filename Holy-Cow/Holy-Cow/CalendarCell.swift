bf760f3f03549255e0b5ecf8b34b3ed3cf4bcbd5
nadiajam <nadiajamrozik2018@u.northwestern.edu>
2016-07-20 20:35:51 +0200

    added calendarCell model

---
+++ Holy-Cow/Holy-Cow/CalendarCell.swift
@@ -0,0 +1,32 @@
+//
+//  CalendarCell.swift
+//  Holy-Cow
+//
+//  Created by Nadia Jamrozik on 7/20/16.
+//  Copyright © 2016 Nadia Jamrozik. All rights reserved.
+//
+
+import UIKit
+
+enum PastDate {
+    case Achieved, Failed, Unset
+}
+
+enum FutureDate {
+    case Meat, Meatless
+}
+
+class CalendarCell {
+    //initialize to empty calendar
+    var calendar = [FutureDate](count: 42, repeatedValue: .Meat)
+    
+    
+    func changePastDate() {
+        
+    }
+    
+    func changeFutureDate() {
+        
+    }
+    
+}
