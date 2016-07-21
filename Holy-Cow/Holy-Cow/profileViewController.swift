b8492e086ec8f1879b916b7f6c11bc6c88768195
nadiajam <nadiajamrozik2018@u.northwestern.edu>
2016-07-21 04:20:56 +0200

    added rain icon as well as number outlets to user view

--- Holy-Cow/Holy-Cow/profileViewController.swift
+++ Holy-Cow/Holy-Cow/profileViewController.swift
@@ -10,20 +10,31 @@ import UIKit
 
 class profileViewController: UIViewController {
 
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var blueView: UIView!
+    @IBOutlet weak var gallonNumberLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        //setting fonts and colors
+        numberLabel.textColor = UIColor.holyPurple
+        numberLabel.font = UIFont(name:"GTWalsheimProTrial-Bold", size: 110.0)
+        blueView.backgroundColor = UIColor.blueColor()
+        
         for subview in view.subviews {
             if let textField = subview as? UILabel {
                 
-                if textField.tag == 2 {     //tag 2 = large number
-                    textField.font = UIFont(name:"GTWalsheimProTrial-Bold", size: 90.0)
-                } else if textField.tag == 4 { //tag 4 = general text
+                if textField.tag == 4 { //tag 4 = general text
                     textField.font = UIFont(name:"GTWalsheimProTrial-Medium", size: 20.0)
                 }
             }
         }
-        // Do any additional setup after loading the view.
+        
+        //modifying profile numbers
+//        numberLabel.text = "\(CalendarController.tallyGoal())"
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
