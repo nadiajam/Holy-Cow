96d6918bed91c9054a2265e9920f41538db42061
Marky <maria_pertsemlidi@brown.edu>
2016-07-21 23:52:21 +0200

    profile view setup, a lot of autolayout problems still

--- Holy-Cow/Holy-Cow/ProgressArc.swift
+++ Holy-Cow/Holy-Cow/ProgressArc.swift
@@ -20,8 +20,8 @@ import UIKit
     override func drawRect(rect: CGRect) {
         super.drawRect(rect)
         let π = 3.141592653589793238462643383279502884197169399
-        let start = 3 * π / 4
-        let end = (3 * π / 4 ) + 2 * π
+        let start = π + π/2
+        let end = 3 * π + π/2
         let progress = progressValue * 2 * π + start
         
         let center = CGPoint(x: bounds.midX, y: bounds.midY)
